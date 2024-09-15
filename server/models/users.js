51bd4203e7942cb6f1354561ac2cccff78aa1bb5
YashMengji <yashmengji2172005@gmail.com>
2024-09-15 16:16:52 +0530

    created schema for user's model

---
+++ server/models/users.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`);
+
+const userSchema = mongoose.Schema({
+  fname: {
+    type: String, 
+    required: true 
+  },
+  lname: {
+    type: String, 
+    required: true 
+  },
+  username: { 
+    type: String, 
+    unique: true, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    unique: true, 
+    required: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  }, // Store hashed password
+  profileImg: {
+    filename: {
+      type: String,
+    },
+    contentType: {
+      type: String, 
+    },
+    data: {
+      type: Buffer,
+    },
+  },
+  friends: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  }], // List of friends (references to other users)
+  friendRequestsSent: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  }], // List of users to whom friend requests have been sent
+  friendRequestsReceived: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  }], // List of users who sent friend requests
+  interests: [String], //  List of interests 
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+})
+
+module.exports = mongoose.model("User", userSchema);
