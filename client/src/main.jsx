70205bd3c65309971e8edd935e5aff8df6658356
YashMengji <yashmengji2172005@gmail.com>
2024-09-15 22:14:17 +0530

    created responsive navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import App from './App.jsx'
+import Register from '../components/Register.jsx'
+
+const createRouter = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App/>,
+      children: [ 
+        {
+          path: "/register", 
+          element: <Register/>
+        }, 
+      ]
+    }
+  ]
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={createRouter}/>
   </StrictMode>,
 )
