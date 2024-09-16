15661abe18f2e6f9900a3e802a731fb27d80a34e
YashMengji <yashmengji2172005@gmail.com>
2024-09-16 23:35:41 +0530

    created user UI

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import User from './User'
 
 function Home() {
   return (
     <div className="home-div">
-      
+      <User />
     </div>
   )
 }
---
+++ client/components/User.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div className="user-div">
+      <div className="div-profile-img">
+        <img src="/images/defaultProfileImg.png" className='profile-img'/>
+      </div>
+      <div className="div-profile-details"> 
+
+      </div>
+      <div className="div-add-friend-btn">
+        
+      </div>
+    </div>
+  )
+}
+
+export default User
